36d4cc096d1de6f32482a11759aaba20cb869141
felipetavaresmelo <felipetavaresmelo@gmail.com>
2020-02-11 08:47:41 -0300

    add cmds

--- assets/scripts/setup-ubuntu.sh
+++ assets/scripts/setup-ubuntu.sh
@@ -4,8 +4,13 @@
 sudo apt-get update &&
 sudo apt-get dist-upgrade -y &&
 
-# Instalacao do VSCODE
+# Instalacao do VSCODE (https://github.com/Microsoft/vscode)
 sudo snap install code --classic
 
-# Instacao do GIMP
+# Instacao do GIMP (https://www.gimp.org/)
 sudo apt-get install gimp &&
+
+# Peek - an animated GIF recorder (https://github.com/phw/peek)
+sudo add-apt-repository ppa:peek-developers/stable &&
+sudo apt update
+sudo apt install peek
