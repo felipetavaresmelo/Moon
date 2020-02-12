045312860801a64e416dec039bf996a8692ccdab
felipetavaresmelo <felipetavaresmelo@gmail.com>
2020-02-12 08:42:08 -0300

    add cmds

--- assets/scripts/setup-ubuntu.sh
+++ assets/scripts/setup-ubuntu.sh
@@ -163,6 +163,13 @@ sudo apt-get whatsdesk_whatsdesk -y
 
 
 # Spotfy
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 931FF8E79F0876134EDDBDCCA87FF9DF48BF1C90
+echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
+sudo apt-get update
+sudo apt-get install spotify-client
+
+sudo apt-get purge spotify_spotify --auto-remove -y
+
 
 # Trello
 
