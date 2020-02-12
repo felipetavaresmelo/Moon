4bbb729635a740862098722acd40fb3ceaca6dba
felipetavaresmelo <felipetavaresmelo@gmail.com>
2020-02-11 22:32:58 -0300

    add cmds

--- assets/scripts/setup-ubuntu.sh
+++ assets/scripts/setup-ubuntu.sh
@@ -4,13 +4,22 @@
 sudo apt-get update &&
 sudo apt-get dist-upgrade -y &&
 
-# Instalacao do VSCODE (https://github.com/Microsoft/vscode)
+# VSCODE (https://github.com/Microsoft/vscode)
 sudo snap install code --classic
 
-# Instacao do GIMP (https://www.gimp.org/)
+# GIMP (https://www.gimp.org/)
 sudo apt-get install gimp &&
+
+# Inkscape (https://wiki.inkscape.org/wiki/index.php/Installing_Inkscape)
+sudo apt-get update &&
+sudo apt-get install inkscape &&
 
 # Peek - an animated GIF recorder (https://github.com/phw/peek)
 sudo add-apt-repository ppa:peek-developers/stable &&
 sudo apt update
 sudo apt install peek
+
+# Dia ()
+sudo apt-get update && 
+sudo apt-get install dia && 
+sudo dpkg -l dia &&  # confirmar instalacao
