338eea0b6dfaf8bcd9c2e363fe332a8170c2d6cb
felipetavaresmelo <felipetavaresmelo@gmail.com>
2020-02-19 02:01:51 -0300

    add cmds

--- assets/scripts/setup-ubuntu.sh
+++ assets/scripts/setup-ubuntu.sh
@@ -178,7 +178,7 @@ sudo apt-get whatsdesk_whatsdesk -y
     sudo apt-get purge telegram --auto-remove -y
 
 
-# Spotfy [quebrado]
+# Spotfy [quebrado] (https://websiteforstudents.com/install-spotify-linux-client-on-ubuntu-16-04-18-04-desktop/)
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 931FF8E79F0876134EDDBDCCA87FF9DF48BF1C90
 echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
 sudo apt-get update
